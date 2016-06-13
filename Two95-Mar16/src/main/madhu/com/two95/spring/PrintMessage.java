f7b53724f072cfa682d0b4dc6dcedf9501a27811
Two95Admin <learner.sm@gmail.com>
2016-06-13 21:40:15 +0530

    Changes and added programs

--- Two95-Mar16/src/main/madhu/com/two95/spring/PrintMessage.java
+++ Two95-Mar16/src/main/madhu/com/two95/spring/PrintMessage.java
@@ -1,10 +1,11 @@
+package com.two95.spring;
 
-public class Welcome {
+public class PrintMessage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
 		System.out.println("Hai..");
-	}	
+	}
 
 }
---
+++ Two95-Mar16/src/main/uday/com/two95/spring/Hai.java
@@ -0,0 +1,10 @@
+package com.two95.spring;
+
+public class Hai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
